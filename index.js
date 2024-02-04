0929372e889eae0291ef0132df578f67cd5146d9
Prodigy-J <jknasumo@gmail.com>
2024-02-04 00:25:30 +0000

    Refactor API endpoints and add error handling

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const app = express();
 
 const Note = require("./models/note");
 
+app.use(express.static("dist"));
 app.use(express.json());
 app.use(cors());
-app.use(express.static("dist"));
 
 const requestLogger = (request, response, next) => {
   console.log("Method: ", request.method);
@@ -17,59 +17,21 @@ const requestLogger = (request, response, next) => {
   next();
 };
 
-app.use(requestLogger);
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message);
 
-// if (process.argv.length < 3) {
-//   console.log("give password as argument");
-//   process.exit(1);
-// }
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
+  }
 
-// const password = process.argv[2];
-
-// const url = `mongodb+srv://jknasumo:${password}@fullstack-mongo-tutoria.nxjpo22.mongodb.net/noteApp?retryWrites=true&w=majority`;
-
-// mongoose.set("strictQuery", false);
-// mongoose.connect(url);
-
-// const noteSchema = new mongoose.Schema({
-//   content: String,
-//   important: Boolean,
-// });
-
-// const Note = mongoose.model("Note", noteSchema);
-
-// noteSchema.set("toJSON", {
-//   transform: (document, returnedObject) => {
-//     returnedObject.id = returnedObject._id.toString();
-//     delete returnedObject._id;
-//     delete returnedObject.__v;
-//   },
-// });
-
-// let notes = [
-//   {
-//     id: 1,
-//     content: "HTML is easy",
-//     important: true,
-//   },
-//   {
-//     id: 2,
-//     content: "Browser can execute only JavaScript",
-//     important: false,
-//   },
-//   {
-//     id: 3,
-//     content: "GET and POST are the most important methods of HTTP protocol",
-//     important: true,
-//   },
-// ];
-
-const generateId = () => {
-  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
-  return maxId + 1;
+  next(error);
 };
 
-app.post("/api/notes", (request, response) => {
+app.use(requestLogger);
+
+app.post("/api/notes", (request, response, next) => {
   const body = request.body;
 
   if (body.content === undefined) {
@@ -83,9 +45,12 @@ app.post("/api/notes", (request, response) => {
     important: body.important || false,
   });
 
-  note.save().then((savedNote) => {
-    response.json(savedNote);
-  });
+  note
+    .save()
+    .then((savedNote) => {
+      response.json(savedNote);
+    })
+    .catch((error) => next(error));
 });
 
 app.get("/api/notes", (request, response) => {
@@ -94,34 +59,35 @@ app.get("/api/notes", (request, response) => {
   });
 });
 
-app.get("/api/notes/:id", (request, response) => {
-  Note.findById(request.params.id).then((note) => {
-    response.json(note);
-  });
+app.get("/api/notes/:id", (request, response, next) => {
+  Note.findById(request.params.id)
+    .then((note) => {
+      if (note) response.json(note);
+      else response.status(404).end();
+    })
+    .catch((error) => next(error));
 });
 
-app.delete("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id);
-  notes = notes.filter((note) => note.id !== id);
-
-  response.status(204).end();
+app.delete("/api/notes/:id", (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
-app.put("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const body = request.body;
+app.put("/api/notes/:id", (request, response, next) => {
+  const { content, important } = request.body;
 
-  if (!body.content) {
-    return response.status(400).json({
-      error: "content missing",
-    });
-  }
-
-  const noteToReplace = notes.find((note) => note.id === id);
-
-  noteToReplace.important = !noteToReplace.important;
-
-  response.json(notes);
+  Note.findByIdAndUpdate(
+    request.params.id,
+    { content, important },
+    { new: true, runValidators: true, context: "query" }
+  )
+    .then((updatedNote) => {
+      response.json(updatedNote);
+    })
+    .catch((error) => next(error));
 });
 
 const unknownEndpoint = (request, response) => {
@@ -129,6 +95,8 @@ const unknownEndpoint = (request, response) => {
 };
 
 app.use(unknownEndpoint);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT);
